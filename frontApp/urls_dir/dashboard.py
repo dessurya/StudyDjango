ecda5da4b5283e7b1ba2c5e672e44f26a57e4749
dessurya <fourline66@gmail.com>
2022-09-01 16:07:54 +0700

    .

--- frontApp/urls_dir/dashboard.py
+++ frontApp/urls_dir/dashboard.py
@@ -4,4 +4,6 @@ from ..views_dir import dashboardView
 urlpatterns = [
     path('', dashboardView.Index, name='pageTo.dashboard'),
     path('sendmail', dashboardView.Sendmail, name='sendmail'),
+    path('createjson', dashboardView.CreateJSON, name='createjson'),
+    path('createimgbase64', dashboardView.CreateIMGBASE64, name='createimgbase64'),
 ]
